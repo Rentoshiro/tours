db4dbb90d134bdaa277f71bbc2eac3273f0fb322
Rentoshiro <149387302+Rentoshiro@users.noreply.github.com>
2024-01-30 21:39:58 +0100

    Update App.jsx and add Loading, Tour, and Tours components
    - Modify src/App.jsx for specific changes
    - Add new files: src/Loading.jsx, src/Tour.jsx, src/Tours.jsx

---
+++ src/Tours.jsx
@@ -0,0 +1,19 @@
+import Tour from "./Tour";
+
+function Tours({ tours }) {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our tours</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="tours">
+        {tours.map((tour) => (
+          <Tour tour={tour.id} {...tour}></Tour>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Tours;
